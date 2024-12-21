85c5faf63ad4093fc659a6457bb0674122d214de
Swadhin <168361244+blueorionn@users.noreply.github.com>
2024-12-21 21:34:23 +0000

    modified gunicorn command

--- Dockerfile
+++ Dockerfile
@@ -50,4 +50,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 EXPOSE 8000
 
 # Start Gunicorn
-CMD ["gunicorn", "wsgi:application", "--bind", "0.0.0.0:8000"]
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "wsgi:app"]
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 # wsgi.py
 from crypticworld import create_app
 
-# 'application' is the WSGI callable that Gunicorn looks for.
-application = create_app()
+# 'app' is the WSGI callable that Gunicorn looks for.
+app = create_app()
