06ad775a0e4f6ec0bf877d696d5e4d55fd5c859c
Swadhin <168361244+blueorionn@users.noreply.github.com>
2024-12-24 05:33:50 +0000

    modified home page

--- crypticworld/core/data.py
+++ crypticworld/core/data.py
@@ -1,16 +1,72 @@
 algorithms = [
-    {"name": "blake2b", "title": "Blake2b", "url": "/hash/blake2b"},
-    {"name": "sha3_256", "title": "SHA3-256", "url": "/hash/sha3_256"},
-    {"name": "sha256", "title": "SHA256", "url": "/hash/sha256"},
-    {"name": "sha224", "title": "SHA224", "url": "/hash/sha224"},
-    {"name": "sha3_224", "title": "SHA3-224", "url": "/hash/sha3_224"},
-    {"name": "sha384", "title": "SHA384", "url": "/hash/sha384"},
-    {"name": "sha512", "title": "SHA512", "url": "/hash/sha512"},
-    {"name": "sha3_512", "title": "SHA3-512", "url": "/hash/sha3_512"},
-    {"name": "blake2s", "title": "Blake2s", "url": "/hash/blake2s"},
-    {"name": "shake_128", "title": "SHAKE128", "url": "/hash/shake_128"},
-    {"name": "shake_256", "title": "SHAKE256", "url": "/hash/shake_256"},
-    {"name": "sha3_384", "title": "SHA3-384", "url": "/hash/sha3_384"},
-    {"name": "sha1", "title": "SHA1", "url": "/hash/sha1"},
-    {"name": "md5", "title": "MD5", "url": "/hash/md5"},
+    {
+        "name": "blake2b",
+        "title": "Blake2b",
+        "url": {"text_hash": "/hash/blake2b", "file_hash": "/hash/file/blake2b"},
+    },
+    {
+        "name": "sha3_256",
+        "title": "SHA3-256",
+        "url": {"text_hash": "/hash/sha3_256", "file_hash": "/hash/file/sha3_256"},
+    },
+    {
+        "name": "sha256",
+        "title": "SHA256",
+        "url": {"text_hash": "/hash/sha256", "file_hash": "/hash/file/sha256"},
+    },
+    {
+        "name": "sha224",
+        "title": "SHA224",
+        "url": {"text_hash": "/hash/sha224", "file_hash": "/hash/file/sha224"},
+    },
+    {
+        "name": "sha3_224",
+        "title": "SHA3-224",
+        "url": {"text_hash": "/hash/sha3_224", "file_hash": "/hash/file/sha3_224"},
+    },
+    {
+        "name": "sha384",
+        "title": "SHA384",
+        "url": {"text_hash": "/hash/sha384", "file_hash": "/hash/file/sha384"},
+    },
+    {
+        "name": "sha512",
+        "title": "SHA512",
+        "url": {"text_hash": "/hash/sha512", "file_hash": "/hash/file/sha512"},
+    },
+    {
+        "name": "sha3_512",
+        "title": "SHA3-512",
+        "url": {"text_hash": "/hash/sha3_512", "file_hash": "/hash/file/sha3_512"},
+    },
+    {
+        "name": "blake2s",
+        "title": "Blake2s",
+        "url": {"text_hash": "/hash/blake2s", "file_hash": "/hash/file/blake2s"},
+    },
+    {
+        "name": "shake_128",
+        "title": "SHAKE128",
+        "url": {"text_hash": "/hash/shake_128", "file_hash": "/hash/file/shake_128"},
+    },
+    {
+        "name": "shake_256",
+        "title": "SHAKE256",
+        "url": {"text_hash": "/hash/shake_256", "file_hash": "/hash/file/shake_256"},
+    },
+    {
+        "name": "sha3_384",
+        "title": "SHA3-384",
+        "url": {"text_hash": "/hash/sha3_384", "file_hash": "/hash/file/sha3_384"},
+    },
+    {
+        "name": "sha1",
+        "title": "SHA1",
+        "url": {"text_hash": "/hash/sha1", "file_hash": "/hash/file/sha1"},
+    },
+    {
+        "name": "md5",
+        "title": "MD5",
+        "url": {"text_hash": "/hash/md5", "file_hash": "/hash/file/md5"},
+    },
 ]
