0271ccddacef74cdf4424ad8d8d595bf63afb29e
Swadhin <168361244+blueorionn@users.noreply.github.com>
2024-12-25 08:10:09 +0000

    added sidebar

--- crypticworld/core/views.py
+++ crypticworld/core/views.py
@@ -1,8 +1,9 @@
 """Core views."""
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
 from flask.views import MethodView
 from .data import algorithms
+from .utils import available_algorithms, get_algorithm
 
 
 blueprint = Blueprint("core", __name__)
@@ -14,5 +15,22 @@ class IndexView(MethodView):
         return render_template("index.html", **data)
 
 
+class StringHashView(MethodView):
+    def get(self, algorithm_name):    
+        if algorithm_name not in available_algorithms():
+            return abort(404)
+
+        data = {
+            "head": {
+                "algorithm_name": get_algorithm(algorithm_name)['title']
+            },
+            "algorithms": algorithms
+        }
+        return render_template("string_hasher.html", **data), 200
+
+
 index_view = IndexView.as_view("home")
 blueprint.add_url_rule("/", view_func=index_view)
+
+string_hash_view = StringHashView.as_view("string_hash")
+blueprint.add_url_rule("/hash/<algorithm_name>", view_func=string_hash_view)
