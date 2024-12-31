5477d0ac359922979561865c260654be70bcb93a
Swadhin <168361244+blueorionn@users.noreply.github.com>
2024-12-31 17:31:19 +0000

    modified sidebar logic

--- crypticworld/static/script/main.js
+++ crypticworld/static/script/main.js
@@ -81,72 +81,34 @@ const themeManager = new ThemeManager();
 class SidebarManager {
   constructor() {
     this.sideBar = document.querySelector('aside[data-sidebar]');
-    this.sideBarToogleButton = document.querySelector('button#sidebar-toogle-button');
+    this.sideBarToogleButton = document.querySelectorAll('button[data-sidebar-toogle-button]');
 
     // If elements not in root exit
     if (!this.sideBar || !this.sideBarToogleButton) return;
 
     this.sideBarClasses = {
-      showSideBar: 'display-sidebar',
-      hideSideBar: 'hide-sidebar',
+      showSideBar: ['display-sidebar', 'flex'],
+      hideSideBar: ['hide-sidebar', 'hidden'],
     };
     this.showclass = this.sideBarClasses.showSideBar;
     this.hideClass = this.sideBarClasses.hideSideBar;
-    this.minWidthToDisplaySideBar = 1280;
-    this.width = window.innerWidth;
-
-    // set sidebar
-    this.#setSideBar();
-
-    // watch for width change
-    window.addEventListener('resize', () => this.#setSideBar());
 
     // toogle sidebar
-    this.sideBarToogleButton.addEventListener('mousedown', () => this.#setSideBar());
-
-    // hide sidebar if clicked outside
-    document.addEventListener('mousedown', (event) => this.#handleClickOutside(event));
+    this.sideBarToogleButton.forEach((ele) => {
+      ele.addEventListener('mousedown', () => this.toogleSideBar());
+    });
 
     // toogle sidebar nav
     this.toogleSidebarNav();
   }
 
-  #setSideBar() {
-    if (this.width < this.minWidthToDisplaySideBar) {
-      if (!this.sideBar.classList.contains(this.showclass)) {
-        this.toogleSideBar();
-      }
-    } else {
-      this.sideBar.classList.remove(this.hideClass);
-    }
-  }
-
   toogleSideBar() {
-    // If width is greater than 1280
-    if (this.width > this.minWidthToDisplaySideBar) return;
-
-    if (this.sideBar.classList.contains(this.showclass)) {
-      this.sideBar.classList.remove(this.showclass, this.hideClass);
-      this.sideBar.classList.add(this.hideClass);
+    if (this.sideBar.classList.contains(...this.showclass)) {
+      this.sideBar.classList.remove(...this.showclass, ...this.hideClass);
+      this.sideBar.classList.add(...this.hideClass);
     } else {
-      this.sideBar.classList.remove(this.showclass, this.hideClass);
-      this.sideBar.classList.add(this.showclass);
-    }
-  }
-
-  #handleClickOutside(event) {
-    // If width is greater than 1280
-    if (this.width > this.minWidthToDisplaySideBar) return;
-
-    // Check if sidebar exists and is open
-    if (this.sideBar.classList.contains(this.hideClass)) return;
-
-    // Check if click is outside both sidebar and toggle button
-    const isClickOutside = !this.sideBar.contains(event.target) && !this.sideBarToogleButton.contains(event.target);
-
-    if (isClickOutside) {
-      this.sideBar.classList.remove(this.showclass);
-      this.sideBar.classList.add(this.hideClass);
+      this.sideBar.classList.remove(...this.showclass, ...this.hideClass);
+      this.sideBar.classList.add(...this.showclass);
     }
   }
 
@@ -180,38 +142,37 @@ const sidebarManager = new SidebarManager();
 // Hash String
 class HashString {
   constructor() {
-    // global scope
-    const globalScope = globalThis
-
     // variables
     this.textArea = document.querySelector('textarea#input');
     this.outputElement = document.querySelector('textarea#output');
     this.digestLenElement = document.querySelector('input#digest_len');
     this.url = '/api/generate_hash/';
     this.defaultDigestLen = 64;
-    
+
     // if elements not in root exit
     if (!this.textArea || !this.outputElement) return;
-    
+
     this.fetchData();
 
     // track digest len
-    this.trackDigestLen();
+    if (this.digestLenElement) {
+      this.trackDigestLen();
+    }
   }
 
-  trackDigestLen(){
+  trackDigestLen() {
     // current algorithm
     let pageUrl = window.location.href.split('/'),
-        algorithm = pageUrl[pageUrl.length - 1];
+      algorithm = pageUrl[pageUrl.length - 1];
 
-    if(['shake_128', 'shake_256'].includes(algorithm)){
+    if (['shake_128', 'shake_256'].includes(algorithm)) {
       // set default value
       this.digestLenElement.value = this.defaultDigestLen;
 
       // watch for change
       this.digestLenElement.addEventListener('input', (event) => {
         this.defaultDigestLen = event.currentTarget.value;
-      })
+      });
     }
   }
 
@@ -250,10 +211,10 @@ class HashString {
           output.textContent = `${data.result}`;
         })
         .catch((error) => {
-          output.textContent = `${error}`
+          output.textContent = `${error}`;
           console.error('Error:', error);
         });
-    }
+    };
 
     // add event listner
     textArea.addEventListener('input', function (event) {
@@ -261,10 +222,12 @@ class HashString {
       timeOutId = setTimeout(fetchApi, delay);
     });
 
-    this.digestLenElement.addEventListener('input', function(event){
-      clearTimeout(timeOutId);
-      timeOutId = setTimeout(fetchApi, delay);
-    })
+    if (this.digestLenElement) {
+      this.digestLenElement.addEventListener('input', function (event) {
+        clearTimeout(timeOutId);
+        timeOutId = setTimeout(fetchApi, delay);
+      });
+    }
 
     return function () {
       clearTimeout(timeOutId);
