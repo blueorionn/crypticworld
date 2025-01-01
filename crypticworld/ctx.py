e23f5ae4be3018bc234a25460de24386cc18e5e3
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-01-01 19:12:43 +0000

    add custom context processor

--- crypticworld/ctx.py
+++ crypticworld/ctx.py
@@ -1,10 +1,27 @@
 """Custom Context Processors."""
 
 import os
-from flask import Flask
+from flask import Flask, current_app
 
 
 def static_context_processors(app: Flask):
     @app.context_processor
     def static_url():
-        return {"static_url": lambda filename: os.path.join("/static", filename)}
+        def process_static_url(filename: str) -> str:
+
+            # If asking for script
+            if filename == "script/index.js" and os.path.isfile(
+                os.path.join("/static", filename)
+            ):
+                return os.path.join("/static", filename)
+            elif filename == "script/index.js" and not os.path.isfile(
+                os.path.join("/static", filename)
+            ):
+                current_app.logger.warn(
+                    f"{filename} Doesn't exist. Returning fallback.js"
+                )
+                return os.path.join("/static", 'script/fallback.js')
+
+            return os.path.join("/static", filename)
+
+        return {"static_url": process_static_url}
