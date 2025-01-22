e3be9110870bce7ff2bb733ae84780ba34cff0df
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-01-22 03:26:01 +0000

    add emoji tests

---
+++ tests/test_file_hash_api/test_emoji_file_hash.py
@@ -0,0 +1,37 @@
+import pytest
+import os
+from flask.testing import FlaskClient
+from crypticworld import create_app
+
+
+@pytest.fixture
+def client():
+    # setting test environment
+    os.environ.setdefault("FLASK_ENV", "testing")
+
+    app = create_app()
+    app.config.update(
+        {
+            "TESTING": True,
+        }
+    )
+    with app.test_client() as client:
+        yield client
+
+
+def test_emoji_file_hash(client: FlaskClient):
+    # file name
+    file_name = "emoji.txt"
+    file_path = os.path.join(os.getcwd(), f"tests/test_file_hash_api/data/{file_name}")
+
+    if not (os.path.exists(file_path)):
+        raise ImportError("File Doesn't exist.")
+
+    with open(file_path, "rb") as f:
+        response = client.post(
+            "/api/file/generate_hash/",
+            headers={"Content-Type": "multipart/form-data"},
+            data={"file": f, "hashing_algorithm": "md5", "encoding_format": "utf-8"},
+        )
+
+    assert response.status_code == 200
