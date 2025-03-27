b663791b043c04c27425f593f1cfa6261ef122c7
Swadhin Tandi <168361244+blueorionn@users.noreply.github.com>
2025-03-27 10:18:21 +0000

    updated context error

--- context/SideBarContext.tsx
+++ context/SideBarContext.tsx
@@ -30,7 +30,7 @@ export const SideBarProvider = ({
 export const useSideBarProvider = (): SideBarContextType => {
   const context = useContext(SideBarContext)
   if (!context) {
-    throw new Error('useSharedState must be used within SharedStateProvider')
+    throw new Error('Unknown Context')
   }
   return context
 }
