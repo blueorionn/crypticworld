ebae60eff6ec4981839238241b42c34965e16fd6
Swadhin Tandi <168361244+blueorionn@users.noreply.github.com>
2025-03-27 10:16:56 +0000

    updated sidebar ui

--- components/(sidebar)/SideBar.tsx
+++ components/(sidebar)/SideBar.tsx
@@ -47,7 +47,7 @@ export function MobileSideBar({
             transition={{ duration: 0.1 }}
             className={`fixed top-0 z-[100] min-h-[100vh] w-full max-w-[15rem] flex-col gap-4 bg-gray-300 px-8 py-6 shadow-md shadow-gray-400 brightness-105 transition-all duration-300 lg:max-w-xs lg:px-10 xl:relative xl:z-50 xl:flex dark:bg-slate-700 dark:shadow-none dark:brightness-90`}
           >
-            <div className='my-6 flex w-full justify-end xl:my-0 xl:hidden'>
+            <div className='my-3 flex w-full justify-end xl:my-0 xl:hidden'>
               <button
                 type='button'
                 className='cursor-pointer'
@@ -63,7 +63,7 @@ export function MobileSideBar({
                 </svg>
               </button>
             </div>
-            <h2 className='text-lg font-bold text-gray-800 lg:text-xl dark:text-gray-400'>
+            <h2 className='my-4 text-lg font-bold text-gray-800 lg:text-xl dark:text-gray-300'>
               Hash
             </h2>
             <div className='flex flex-col'>
@@ -100,19 +100,7 @@ export function DesktopSideBar({ hashUrl }: { hashUrl: string }) {
       <aside
         className={`fixed top-0 z-[100] min-h-[100vh] w-full max-w-[15rem] flex-col gap-4 bg-gray-300 px-8 py-6 shadow-md shadow-gray-400 brightness-105 transition-all duration-300 lg:max-w-xs lg:px-10 xl:relative xl:z-50 xl:flex dark:bg-slate-700 dark:shadow-none dark:brightness-90`}
       >
-        <div className='my-6 flex w-full justify-end xl:my-0 xl:hidden'>
-          <button type='button' className='cursor-pointer'>
-            <span className='sr-only'>Close Button</span>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-6 w-6 fill-gray-700 stroke-gray-700 dark:fill-gray-300 dark:stroke-gray-300'
-              viewBox='0 -960 960 960'
-            >
-              <path d='m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z'></path>
-            </svg>
-          </button>
-        </div>
-        <h2 className='text-lg font-bold text-gray-800 lg:text-xl dark:text-gray-400'>
+        <h2 className='my-4 text-lg font-bold text-gray-800 lg:text-xl dark:text-gray-300'>
           Hash
         </h2>
         <div className='flex flex-col'>
